bd75dfbccc62ba18ce4519479f65d32662ae44f4
Veer Rajpoot <rahulksingh3907@gmail.com>
2024-05-07 01:30:30 +0530

    refactor: Update Loader component and add new Loader variant

--- src/components/SharedComponents/Loader.jsx
+++ src/components/SharedComponents/Loader.jsx
@@ -1,19 +1,19 @@
-function Loader() {
-    return (
-        <div>
-            <div className="animate-pulse flex space-x-4">
-                <div className="rounded-full bg-gray-300 h-12 w-12"></div>
-                <div className="flex-1 space-y-4 py-1">
-                    <div className="h-4 bg-gray-300 rounded w-3/4"></div>
-                    <div className="space-y-2">
-                        <div className="h-4 bg-gray-300 rounded"></div>
-                        <div className="h-4 bg-gray-300 rounded w-5/6"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    );
-}
+// function Loader() {
+//     return (
+//         <div>
+//             <div className="animate-pulse flex space-x-4">
+//                 <div className="rounded-full bg-gray-300 h-12 w-12"></div>
+//                 <div className="flex-1 space-y-4 py-1">
+//                     <div className="h-4 bg-gray-300 rounded w-3/4"></div>
+//                     <div className="space-y-2">
+//                         <div className="h-4 bg-gray-300 rounded"></div>
+//                         <div className="h-4 bg-gray-300 rounded w-5/6"></div>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
 
 function CategoriesLoader() {
     return (
@@ -73,6 +73,14 @@ function JobCardLoader() {
                     <div className="h-4 bg-gray-300 rounded"></div>
                 </div>
             </div>
+        </div>
+    );
+}
+
+function Loader() {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <i className="fas fa-spinner fa-spin text-4xl text-[color:var(--primary-color)]"></i>
         </div>
     );
 }
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -7,84 +7,98 @@
 //     usertype: "User";
 // }
 
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import UserContext from "../context/userContext";
 import { getUnsplashImage } from "../utils/UnsplashImage";
+import Loader from "../components/SharedComponents/Loader";
 
 function UserProfile() {
     const { user } = useContext(UserContext);
     const [coverImage, setCoverImage] = useState("");
-    getUnsplashImage("cover").then((data) => {
-        setCoverImage(data.urls.regular);
-    });
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setLoading(true);
+        if (user?.user_id) {
+            getUnsplashImage("cover").then((data) => {
+                setCoverImage(data.urls.regular);
+                setLoading(false);
+            });
+        }
+    }, [user?.user_id]);
+
     return (
         <div>
-            <div className="max-w-7xl m-auto px-4 md:px-8">
-                <div
-                    className="w-full -z-10 bg-slate-500 h-28 md:h-44 lg:h-64 bg-center bg-cover bg-no-repeat relative rounded-lg mt-8"
-                    style={{ backgroundImage: `url(${coverImage})` }}
-                ></div>
+            {!loading && (
+                <div className="max-w-7xl m-auto px-4 md:px-8">
+                    <div
+                        className="w-full -z-10 bg-slate-500 h-28 md:h-44 lg:h-64 bg-center bg-cover bg-no-repeat relative rounded-lg mt-8"
+                        style={{ backgroundImage: `url(${coverImage})` }}
+                    ></div>
 
-                <div className="rounded-lg -mt-16 md:-mt-24 lg:-mt-32 p-4 md:p-8">
-                    <div className="flex justify-center">
-                        <img
-                            className="h-24 w-24 md:h-32 md:w-32 lg:h-48 lg:w-48 rounded-full"
-                            src={user.user_image}
-                            alt={user.name}
-                        />
+                    <div className="rounded-lg -mt-16 md:-mt-24 lg:-mt-32 p-4 md:p-8">
+                        <div className="flex justify-center">
+                            <img
+                                className="h-24 w-24 md:h-32 md:w-32 lg:h-48 lg:w-48 rounded-full"
+                                src={user?.user_image}
+                                alt={user?.name}
+                            />
+                        </div>
                     </div>
-                </div>
-                <div className="mt-4 md:mt-8 lg:mt-12">
-                    <div className="text-center">
-                        <h1 className="text-2xl md:text-4xl lg:text-5xl font-semibold text-[color:var(--primary-color)]">
-                            {user.name}
-                        </h1>
-                        <p className="text-lg md:text-xl lg:text-2xl text-[color:var(--secondary-color)]">
-                            {user.email}
-                        </p>
-                    </div>
+                    <div className="mt-4 md:mt-8 lg:mt-12">
+                        <div className="text-center">
+                            <h1 className="text-2xl md:text-4xl lg:text-5xl font-semibold text-[color:var(--primary-color)]">
+                                {user?.name}
+                            </h1>
+                            <p className="text-lg md:text-xl lg:text-2xl text-[color:var(--secondary-color)]">
+                                {user?.email}
+                            </p>
+                        </div>
 
-                    <div className="mt-8">
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                            <div className="bg-white rounded-lg p-4">
-                                <h2 className="text-xl font-semibold text-[color:var(--primary-color)]">
-                                    Contact Information
-                                </h2>
-                                <p className="text-[color:var(--secondary-color)]">
-                                    <span className="font-semibold">
-                                        Email:
-                                    </span>{" "}
-                                    {user.email}
-                                </p>
-                                <p className="text-[color:var(--secondary-color)]">
-                                    <span className="font-semibold">
-                                        Phone:
-                                    </span>{" "}
-                                    {user.phone}
-                                </p>
-                            </div>
+                        <div className="mt-8">
+                            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                                <div className="bg-white rounded-lg p-4">
+                                    <h2 className="text-xl font-semibold text-[color:var(--primary-color)]">
+                                        Contact Information
+                                    </h2>
+                                    <p className="text-[color:var(--secondary-color)]">
+                                        <span className="font-semibold">
+                                            Email:
+                                        </span>{" "}
+                                        {user?.email}
+                                    </p>
+                                    <p className="text-[color:var(--secondary-color)]">
+                                        <span className="font-semibold">
+                                            Phone:
+                                        </span>{" "}
+                                        {user?.phone}
+                                    </p>
+                                </div>
 
-                            <div className="bg-white rounded-lg p-4">
-                                <h2 className="text-xl font-semibold text-[color:var(--primary-color)]">
-                                    User Information
-                                </h2>
-                                <p className="text-[color:var(--secondary-color)]">
-                                    <span className="font-semibold">
-                                        User ID:
-                                    </span>{" "}
-                                    {user.user_id}
-                                </p>
-                                <p className="text-[color:var(--secondary-color)]">
-                                    <span className="font-semibold">
-                                        User Type:
-                                    </span>{" "}
-                                    {user.usertype}
-                                </p>
+                                <div className="bg-white rounded-lg p-4">
+                                    <h2 className="text-xl font-semibold text-[color:var(--primary-color)]">
+                                        User Information
+                                    </h2>
+                                    <p className="text-[color:var(--secondary-color)]">
+                                        <span className="font-semibold">
+                                            User ID:
+                                        </span>{" "}
+                                        {user?.user_id}
+                                    </p>
+                                    <p className="text-[color:var(--secondary-color)]">
+                                        <span className="font-semibold">
+                                            User Type:
+                                        </span>{" "}
+                                        {user?.usertype}
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            )}
+
+            {loading && <Loader />}
         </div>
     );
 }
